e6be0d7daea00eb1bda48a504c738d29ca5e18c3
Mikael Vaikmae <mikael.vaikmae@gmail.com>
2024-07-08 21:04:54 +0300

    graph hover effect

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,36 +1,38 @@
 <script lang="ts">
-    import { goto, invalidate } from '$app/navigation';
-    import { onMount } from 'svelte';
+	import { goto, invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
 
-    async function signout() {
-        const response = await fetch('?/logout', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: JSON.stringify({
-                action: 'logout'
-            })
-        });
+	async function signout() {
+		const response = await fetch('?/logout', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			body: JSON.stringify({
+				action: 'logout'
+			})
+		});
 
-        if (response.ok) {
-            // Invalidate the data and reload the page to reflect the signout
-            await invalidate('/dashboard');
-            await goto('/');
-            console.log("great succcesss")
-        } else {
-            console.error('Signout failed', response); 
-        }
-    }
+		if (response.ok) {
+			// Invalidate the data and reload the page to reflect the signout
+			await invalidate('/dashboard');
+			await goto('/');
+			console.log('great succcesss');
+		} else {
+			console.error('Signout failed', response);
+		}
+	}
 
-    onMount(() => {
-    });
-
+	onMount(() => {});
 </script>
 
 <div class="flex justify-between">
-    <slot />
-    <nav class="h-min m-2">
-        <button on:click={signout} class="w-16 h-16 p-2 border mb-5 drop-shadow-md bg-gradient-to-l from-blue-500/80 to-purple-500/50 rounded-xl pointer text-white">Sign Out</button>
-    </nav>
+	<slot />
+	<nav class="h-min m-2">
+		<button
+			on:click={signout}
+			class="w-16 h-16 p-2 border mb-5 drop-shadow-md bg-gradient-to-l from-blue-500/80 to-purple-500/50 rounded-xl pointer text-white"
+			>Sign Out</button
+		>
+	</nav>
 </div>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,40 +1,53 @@
 <script lang="ts">
-    // Dashboard logic
-    import { onMount } from 'svelte';
+	// Dashboard logic
+	import { onMount } from 'svelte';
 	import toast from 'svelte-french-toast';
-    import { enhance } from '$app/forms';
-    import type { Result } from '$lib/types/types';
+	import { enhance } from '$app/forms';
+	import type { Result } from '$lib/types/types';
 
 	export let data: Result;
 
-    const firstName = data.user.name;
+	const firstName = data.user.name;
 </script>
 
 <div class="flex flex-col grow content-center">
-    <h1 class="text-4xl size-fit m-2">Dashboard</h1>
-    <h2>{firstName}</h2>
-    <h2>dein auditRatio, Man: {data.user.auditRatio}</h2>
+	<h1 class="text-4xl size-fit m-2">Dashboard</h1>
+	<h2>{firstName}</h2>
+	<h2>dein auditRatio, Man: {data.user.auditRatio}</h2>
 </div>
 
 <div class="flex flex-col">
-<svg class="bg-white" width="auto" height="auto" role="img">
-    <title class="text-white bg-black">Audits</title>
-    <g>
-        <rect width="{data.auditSumUp/10000}" height="30" class="fill-current text-green-500"></rect>
-        <text y="30">Up</text>
-    </g>
-    <g transform="translate(0, 30)"> 
-        <rect width="{data.auditSumDown/10000}" height="30" class="fill-current text-red-500"></rect>
-        <text y="30">Down</text>
-    </g>
-</svg>
+	<svg class="rounded-lg p-px bg-gray-500" width="auto" height="auto" role="img">
+		<title class="text-white bg-black">Audits</title>
+		<g>
+			<rect width={data.auditSumUp / 10000} height="30" class="fill-current text-green-500"></rect>
+			<text y="30">Up</text>
+		</g>
+		<g transform="translate(0, 30)">
+			<rect width={data.auditSumDown / 10000} height="30" class="fill-current text-red-500"></rect>
+			<text y="30">Down</text>
+		</g>
+	</svg>
 </div>
 
-<svg class="bg-sky-500" width="auto" height="auto">
-    {#each data.XP_Transfers as project, i}
-    <g>
-<rect y="{i*25}" width="{project.amount/100}" height="20" class="fill-current text-gray-500"></rect>
-    </g>
-    <text y="{i*26}">{project.object.name}</text>
-    {/each}
+<svg class="bg-sky-500 m-2 rounded-lg pt-4" width="auto" height="540">
+	{#each data.XP_Transfers as project, i}
+		<g transform={`translate(0, ${i * 30})`} class="group">
+			<rect
+				width={project.amount / 200}
+				height="20"
+				class="fill-current text-gray-200 hover:text-slate-700 transition-all"
+			></rect>
+			<text x={project.amount / 200 + 5} y="15" class="group-hover:scale-0">
+				{project.object.name}
+			</text>
+			<text
+				x={project.amount / 200 + 5}
+				y="15"
+				class="text-white scale-0 group-hover:scale-100 bg-black"
+			>
+				{project.amount / 1000} kB
+			</text>
+		</g>
+	{/each}
 </svg>
