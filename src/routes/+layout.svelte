7f6ca3325a5e4cd83fbe95ad992aa82e21defa54
heinztamm <karl_tamm@hotmail.com>
2024-07-10 16:40:52 +0300

    a broken deployment checkpoint

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,8 @@
 <script>
-<<<<<<< HEAD
 	import "../app.css";
 	  import { Toaster } from 'svelte-french-toast';
 	import { onMount } from 'svelte';
 	import toast from 'svelte-french-toast';
-=======
-  import "../app.css";
-  import './styles.css';
-	import { Toaster } from 'svelte-french-toast';
-  import { onMount } from 'svelte';
-  import toast from 'svelte-french-toast';
-
-      // Function to check for the 'toast' cookie and display it
-      function checkForToast() {
-        const cookies = document.cookie.split('; ');
-        const toastCookie = cookies.find(row => row.startsWith('toast='));
-        if (toastCookie) {
-            const message = decodeURIComponent(toastCookie.split('=')[1]);
-            toast(message, { duration: 7000 });
-            // Remove the toast cookie after displaying it
-            document.cookie = 'toast=; Max-Age=0; path=/';
-        }
-    }
-
-    onMount(() => {
-        checkForToast();
-    });
-  </script>
->>>>>>> trying to deploy to netlify
   
 		// Function to check for the 'toast' cookie and display it
 		function checkForToast() {
